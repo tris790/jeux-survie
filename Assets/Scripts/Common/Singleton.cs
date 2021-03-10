4ad4005546d0dc27fd11b4727dc166c657677aa6
Etienne G-C <etienne.gauvin-clermont@usherbrooke.ca>
2021-03-10 02:34:56 -0500

    Add scripts to move player in  fix area

---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static bool m_isQuitting;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                // Making sure that there's not other instances of the same type in memory.
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    // Initializing a new game object to contain a singular instance.
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            // If null, this instance is now the Singleton instance of the assigned type.
+            _instance = this as T;
+
+            // Making sure that Singleton instance will persist in memory across every scene.
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            // Destroy current instance because it must be a duplicate.
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Components/MovementComponent.cs
@@ -0,0 +1,65 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using UnityEngine;
+
+[AddComponentMenu("Components/Movement Component")]
+public class MovementComponent : MonoBehaviour, IMoveable, IRotable
+{
+    public event Action<bool> AnimateMovement = delegate { };
+
+    [HideInInspector]
+    public float moveSpeed;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _moveDirection;
+    private bool _isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        AnimateMovement(_isMoving);
+        Rotate();
+    }
+
+    // TODO : EGC move this in a InputComponent
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        _isMoving = (moveX != 0 || moveY != 0);
+        _moveDirection = new Vector2(moveX, moveY);
+    }
+
+    public void Move()
+    {
+        _rigidbody.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+
+    public void Rotate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        _rigidbody.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
