47a9972a97049be6d084533151c9c7956a5a603a
minicarter <minicarter1523@hotmail.com>
2021-04-08 13:10:26 -0400

    Add camera

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera _camera;
+    public GameObject _objectToFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraFollow();
+    }
+
+    void CameraFollow()
+    {
+        float charPosX = _objectToFollow.transform.position.x;
+        float charPosZ = _objectToFollow.transform.position.z - 1;
+        float cameraOffset = _objectToFollow.transform.position.y;
+
+        _camera.transform.position = new Vector3(charPosX, cameraOffset, charPosZ);
+    }
+}
